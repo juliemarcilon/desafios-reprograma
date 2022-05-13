5693b27e7b630557751fdc440143bb2b927d4d55
julie <lilianamarcilon@MacBook-Air-de-Liliana-2.local>
2022-05-12 23:07:29 -0300

    Entrega desafios 1 e 2

--- src/js/script.js
+++ src/js/script.js
@@ -26,12 +26,55 @@ const users = [
     }
 ];
 
-function ValidateForm() {
-    /*
-        1. Referenciar elementos dos campos do formulário pelo ID
-        2. Verificar se a senha e o e-mail estão válidos 
-        3. Se tudo estiver ok, alertar o usuário que o formulário foi submetido
+// DESAFIO 1
+function somaIdade(){
+    const idade = users.map(user => user.age);
+    const soma = idade.reduce((previous, current) => previous + current, 0);
+    console.log("A soma das idades é " + soma);
+}
+// somaIdade();
 
-        Plus: Alertar o usuário quando o e-mail ou senha estiverem vazios ou incorretos.
-    */
+
+function validaLogin(parametro){
+    const login = users.find(user => user.login === parametro);
+
+    if (login === undefined){
+        console.log('Usuário ' + login.login + ' não encontrado');
+    }else{
+        console.log('Usuário ' + login.login + ' encontrado');
+    }
+}
+// validaLogin('brunohs');
+
+
+function mostraEmails(){
+    const emails = users.map(user => user.email);
+    console.log("Lista de emails: " + emails);
+}
+// mostraEmails();
+
+
+function contaEmailsPb(){
+    const emailsPb = users.filter(user => user.email.endsWith('@pbtech.net.br'));
+    console.log(emailsPb.length + " usuários possuem o email da empresa");
+}
+// contaEmailsPb();
+
+
+// DESAFIO 2
+function validateForm(event){
+    let emailDigitado = event.target.elements.namedItem("email").value;
+    let senhaDigitada = event.target.elements.namedItem("passwd").value;
+    const usuario = users.find(user => user.email === emailDigitado);
+    const idades = users.map(user => user.age);
+    const soma = idades.reduce((previous, current) => previous + current, 0);
+    if (!emailDigitado || !senhaDigitada) {
+        alert('Preencher usuário e senha');
+    } else if (!usuario || (senhaDigitada != soma)) {
+        alert('Usuário ou senha incorretos');
+    } else {
+        alert('Formulário submetido');
+    }
+
+    return false;
 }
